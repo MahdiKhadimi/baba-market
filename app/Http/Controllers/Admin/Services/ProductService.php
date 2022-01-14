a2c6c28f73bedb61c461bbabdeeb293293fee748
mahdi khadimi <mahdikhadimi14@gmail.com>
2022-01-14 16:13:54 +0430

    bug-fix product store

--- app/Http/Controllers/Admin/Services/ProductService.php
+++ app/Http/Controllers/Admin/Services/ProductService.php
@@ -21,7 +21,9 @@ class ProductService extends Controller
      */
     public static function create(Request $request)
     {
-          
+
+           $request['active'] = $request->has('active') ? true : false;  
+
             //get upload image Name
             $request['image'] = uploadService::handle($request->file('cover'), config('shop.productCoverPath'), 'productCover');
 
