481d6d2e9a7b70719e69c0f7db24eab3c518db2d
mahdi khadimi <mahdikhadimi14@gmail.com>
2022-02-22 16:08:50 +0430

    Bug-fix config part 2: a few change at code in config folder

--- config/shop.php
+++ config/shop.php
@@ -10,15 +10,14 @@ return [
      */
     'perPage'           => 10,
 
-
     /*
      |------------------------------
      | OTP config
      |------------------------------
      */
-    'otp_sec' =>120 , // every otp is valid unitil specified second
-    'otp_wait'=>'Wait until 120 seconds' ,
-    'otp_succ' =>'YOUR OTP CODE IS :',
+    'otp_sec'           => 120, // every otp is valid unitil specified second
+    'otp_wait'          => 'Wait until 120 seconds',
+    'otp_succ'          => 'YOUR OTP CODE IS :',
 
 
     /*
@@ -29,42 +28,53 @@ return [
      |
      |
      */
-
-
     'brandImagePath'    => 'images\\brands\\',
-    'discountImagePath'    => 'images\\discounts\\',// path for save discount images
+    'discountImagePath' => 'images\\discounts\\',//path for saving discount image ,
     'productCoverPath'  => 'images\\products_cover\\',//path for saving product main image which will be shown in the fornt end
     'productGalleris'   => 'images\\product_galleries\\',//path to save product gallery image
 
-
+    /*
+     |------------------------------
+     | CRUD Message
+     |------------------------------
+     |
+     |
+     |
+     */
     'msg'               => [
-               'create'      => 'successfully created',
-                'update'      => 'successfully updated',
-                'delete'      => 'successfully Delete',
-                'delete_fail' => 'Fail While Deleting',
-        
-                'fail_update' => 'Faile While Updating',
-                'fail'        => 'UserName | Password is Invalid',
-                'fail_create'  =>'Fail While Creating new Product',
-        
-                'add_wishlist'       => 'product successfully added to your wishlist ',
-                'was_exist_wishlist' => 'product was exist in your wishlist',
-        
-                'add_basket' => 'Product Successfully Added To Your Basket',
-        
-                'increase_count'      => 'Product Successfully Increased +1',
-                'increase_count_fail' => 'Increase Was Fail / try Againg',
-                        'dec_count_succ' => 'Product Successfully Decreased -1',
-                'dec_count_fail' => 'Product Failed Decreased -1',
-        
-                'coupon_expired' => 'The Coupon Code Is Not Valid!',
-                'fail_status_order' => 'somthing goes wrong!',
-                'empty_search' => 'The Order Search Result Was Empty',
-    
-               'succ_comment' =>'Your Comment successfully sbmited , after review it will be shown ' ,
-              'fail_commment' =>'Somthing Goes Wrong , try again' ,
-    
-               'succ_reply' =>'Successfully Replied' ,
-              'fail_reply'=> 'fail while Repling',
+        'create'      => 'successfully created',
+        'update'      => 'successfully updated',
+        'delete'      => 'successfully Delete',
+        'delete_fail' => 'Fail While Deleting',
+
+        'fail_update' => 'Faile While Updating',
+        'fail'        => 'UserName | Password is Invalid',
+        'fail_create'  =>'Fail While Creating new Product',
+
+        'add_wishlist'       => 'product successfully added to your wishlist ',
+        'was_exist_wishlist' => 'product was exist in your wishlist',
+
+        'add_basket' => 'Product Successfully Added To Your Basket',
+
+        'increase_count'      => 'Product Successfully Increased +1',
+        'increase_count_fail' => 'Increase Was Fail / try Againg',
+
+        'dec_count_succ' => 'Product Successfully Decreased -1',
+        'dec_count_fail' => 'Product Failed Decreased -1',
+
+        'coupon_expired' => 'The Coupon Code Is Not Valid!',
+
+        'fail_status_order' => 'somthing goes wrong!',
+
+        'fail_update_order_status' => 'Fail while Updating Order Status / try a again ...',
+
+        'empty_search' => 'The Order Search Result Was Empty',
+
+        'succ_comment' =>'Your Comment successfully sbmited , after review it will be shown ' ,
+        'fail_commment' =>'Somthing Goes Wrong , try again' ,
+
+        'succ_reply' =>'Successfully Replied' ,
+        'fail_reply'=> 'fail while Repling',
+
     ]
 ];
